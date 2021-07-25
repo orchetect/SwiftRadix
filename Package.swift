421c58b4035d3b033717245b3ec75f3f34da2cb4
Steffan Andrews <stef@orchetect.com>
2021-07-24 22:58:47 -0700

    Formatting

--- Package.swift
+++ Package.swift
@@ -9,29 +9,23 @@ let package = Package(
 	platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
 	
 	products: [
-		
 		.library(
 			name: "SwiftRadix",
 			targets: ["SwiftRadix"])
-		
 	],
 	
 	dependencies: [
-		
 		// none
-		
 	],
 	
 	targets: [
-		
 		.target(
 			name: "SwiftRadix",
 			dependencies: []),
 		
 		.testTarget(
 			name: "SwiftRadixTests",
-			dependencies: ["SwiftRadix"])
-		
+			dependencies: ["SwiftRadix"])		
 	]
 	
 )
