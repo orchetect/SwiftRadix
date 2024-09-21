958cfee070308f43c0781732eaf248f85159c485
Steffan Andrews <stef@orchetect.com>
2024-09-21 13:39:10 -0700

    Removed stale unit test file

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LinuxMain.swift
-//  SwiftRadix • https://github.com/orchetect/SwiftRadix
-//  © 2022 Steffan Andrews • Licensed under MIT License
-//
-
-fatalError("Run the tests with `swift test --enable-test-discovery`")
